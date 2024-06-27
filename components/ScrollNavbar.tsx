09ca676661286603b3ed35b218d92d0e70f63c05
aniketSingh0310 <aniketfcb10@gmail.com>
2024-06-27 16:22:18 +0530

    .

--- components/ScrollNavbar.tsx
+++ components/ScrollNavbar.tsx
@@ -9,11 +9,12 @@ import { useRouter } from "next/navigation";
 import { RxHamburgerMenu } from "react-icons/rx";
 import Logo from "./logo";
 import localFont from 'next/font/local'
+import { Download, Menu, X } from "lucide-react";
 
 
 //font
 const myFont = localFont({
-  src: '../font/pilatDemi.ttf',
+  src: '../font/pilatLight.ttf',
   display: 'swap',
 })
 const raleway = Raleway({
@@ -23,12 +24,14 @@ const raleway = Raleway({
 
 
 const ScrollNavbar = () => {
-  const router= useRouter();
+  const router = useRouter();
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
+  const [scrollPosition, setScrollPosition] = useState(0);
 
   const handleScroll = () => {
     const currentScrollPos = window.scrollY;
+    setScrollPosition(window.scrollY);
     const isScrolledDown = prevScrollPos < currentScrollPos;
 
     setPrevScrollPos(currentScrollPos);
@@ -47,99 +50,125 @@ const ScrollNavbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleNav = () => {
-    setIsOpen(!isOpen);
+    console.log("Hamburger clicked");
+    console.log("Current isOpen state:", isOpen); // Log the current state
+    setIsOpen(!isOpen); // Toggle the isOpen state based on previous state
+    console.log("Updated isOpen state:", !isOpen);
   };
 
-  const homFun=()=>{
+  const homFun = () => {
     setIsOpen(!isOpen);
-    router.push("/")
-
-  }
-  const aboutFun=()=>{
+    router.push("/");
+  };
+  const productsFun = () => {
     setIsOpen(!isOpen);
-    router.push("/about")
-
-  }
-  const contactFun=()=>{
+    router.push("/products");
+  };
+  const contactFun = () => {
     setIsOpen(!isOpen);
-    router.push("/contact-us")
-
-  }
+    router.push("/contact-us");
+  };
+  const aboutFun = () => {
+    setIsOpen(!isOpen);
+    router.push("/about");
+  };
   return (
     <>
-    <motion.nav
-      className={`fixed top-0 w-full  transition-all duration-300 z-[100] hidden md:block`}
-      initial={{ y: -100 }}
-      animate={{ y: visible ? 0 : -100 }}
-    >
-      <div className={`w-full py-6 bg-transparent backdrop-blur-sm `}>
-        <div className="flex flex-row px-16 justify-between items-center">
-         <Logo/>
-          <div className="flex">
-            {" "}
-            <div className="flex items-center md:justify-center md:space-x-10">
-              <ul className={cnx("flex space-x-12 font-medium")}>
-                <Link href={"/"}> 
-                  {" "}
-                  <button>
-                    <li>Home</li>
-                  </button>
-                </Link>
-                <Link href={"/about"}>
-                  <button>
-                    <li>About</li>
-                  </button>
-                </Link>
-                <Link href={"/contact-us"}>
-                  <button>
+      {/* DEKSTOP VIEW */}
+      <motion.nav
+        className={`fixed top-0 w-full  transition-all duration-300 z-[100] hidden md:block ${
+          scrollPosition > 0 ? "bg-black" : "bg-transparent"
+        } ease-in duration-150 transition-all`}
+        initial={{ y: -100 }}
+        animate={{ y: visible ? 0 : -100 }}
+      >
+        <div className={`w-full py-5  bg-transparent`}>
+          <div className="flex flex-row px-16 justify-between items-center">
+            <Logo />
+            <div className="flex">
+              {" "}
+              <div className="flex items-center md:justify-center md:space-x-10">
+                <ul
+                  className={cn(
+                    "flex justify-center items-center gap-[50px] text-[12px] text-white tracking-wide",myFont.className
+                  )}
+                >
+                  <Link href={"/"}>
+                    {" "}
+                    <li className="">Home</li>
+                  </Link>
+                  <Link href={"/services"}>
+                    <li className="">Services</li>
+                  </Link>
+                  <Link href={"/about"}>
+                    <li className="">About Us</li>
+                  </Link>
+                  <Link href={"/contact-us"}>
                     <li>Contact Us</li>
-                  </button>
-                </Link>
-                
-              </ul>
-              
+                  </Link>
+                  
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </motion.nav>
-<motion.nav className="flex flex-col gap-0 fixed top-0 w-full  transition-all duration-300 z-[100]"
-initial={{ y: -100 }}
-animate={{ y: visible ? 0 : -100 }}>
-    <div className={cn("md:hidden w-full bg-black/10 backdrop-blur-md shadow-md flex flex-row justify-between py-7 px-3 items-center")}>
-    <Logo/>
-    <div>
-    <RxHamburgerMenu onClick={toggleNav} className="w-[30px] h-[30px] text-white"/>
-    
-    </div>
-    </div>
-    <div className="relative">
-    {isOpen && (
-        <motion.div className={cn("h-max z-50 inset-x-0 md:hidden w-full bg-black/10 backdrop-blur-md shadow-md py-3 px-3 transition-opacity duration-1000 opacity-100 rounded-b-3xl")}
-        initial={{ y: 0 }}
-      animate={{ y: isOpen ? 0 : -300 }}>
-          <ul className={cnx("flex flex-col space-y-5 font-bold justify-center items-center text-white",myFont.className)}>
-                
-                  {" "}
-                  <button onClick={homFun}> 
-                    <li className="">Home</li>
-                  </button>
-                
-                
-                  <button onClick={aboutFun}>
-                    <li className="">About</li>
-                  </button>
-                
-                
-                  <button onClick={contactFun}>
-                    <li className="">Contact Us</li>
-                  </button>
-                
-                
+      </motion.nav>
+
+      {/* MOBILE VIEW */}
+      <motion.nav
+        className="flex flex-col gap-0 fixed top-0 w-full  transition-all duration-300 z-[100] bg-black/40 backdrop-blur-md"
+        initial={{ y: -100 }}
+        animate={{ y: visible ? 0 : -100 }}
+      >
+        <div
+          className={cn(
+            "md:hidden w-full shadow-md flex flex-row justify-between py-5 px-5 items-center"
+          )}
+        >
+          <Logo />
+          <div className="flex items-center gap-2">
+          <button
+                    className="flex items-center gap-2 hover:brightness-110 hover:animate-pulse py-2 px-5 rounded-full bg-indigo-500 shadow-lg shadow-indigo-500/50 text-white font-semibold text-sm"
+                    onClick={() => window.open("/brochure.pdf", "_blank")}
+                  >
+                    <Download size={14} /> Brochure
+                  </button>            {!isOpen ? (
+              <Menu size={30} className="text-white" onClick={toggleNav} />
+            ) : (
+              <X size={30} className="text-white" onClick={toggleNav} />
+            )}
+          </div>
+        </div>
+        <div className="relative">
+          {isOpen && (
+            <motion.div
+              className={cn(
+                "h-max z-50 inset-x-0 md:hidden w-full bg-black/40 backdrop-blur-md shadow-lg py-3 px-3 transition-opacity duration-1000 opacity-100"
+              )}
+              initial={{ y: -20 }} // Start off-screen
+              animate={{ y: isOpen ? 0 : -300 }}
+              transition={{ duration: 0.5, type: "tween" }} // Adjust the duration here (in seconds)
+            >
+              <ul
+                className={cn(
+                  "flex flex-col space-y-5  text-white text-[18px] font-medium text-start px-7 pb-6 tracking-wide"
+                )}
+              >
+                {" "}
+                <li className="border-b-[1px] border-b-black/10 pb-1">Home</li>
+                <li className="border-b-[1px] border-b-black/10 pb-1">
+                  Products
+                </li>
+                <li className="border-b-[1px] border-b-black/10 pb-1">
+                  About Us
+                </li>
+                <li className="border-b-[1px] border-b-black/10 pb-1">
+                  Contact Us
+                </li>
               </ul>
-        </motion.div>
-      )}
-      </div>
+            </motion.div>
+          )}
+        </div>
       </motion.nav>
     </>
   );
